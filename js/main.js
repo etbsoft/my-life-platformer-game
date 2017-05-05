1c06744b4f603cb6244f0db34329293cc0a46873
Isabel Muñoz <teanocrata@gmail.com>
2017-05-05 09:59:34 +0200

    Load the level data

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 PlayState = {}
 
 PlayState.preload = function () {
+  this.game.load.json('level:1', 'data/level01.json')
   this.game.load.image('background', 'images/background.png')
 }
 
 PlayState.create = function () {
   this.game.add.image(0, 0, 'background')
+  this._loadLevel(this.game.cache.getJSON('level:1'))
+}
+
+PlayState._loadLevel = function (data) {
+  console.log(data)
 }
 
 window.onload = function () {
