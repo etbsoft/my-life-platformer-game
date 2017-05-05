a7df9f88282c325a825368699cc97891491b3dc6
Isabel Muñoz <teanocrata@gmail.com>
2017-05-05 11:23:29 +0200

    Create custom sprite for the enemies

--- js/main.js
+++ js/main.js
@@ -26,6 +26,28 @@ Hero.prototype.jump = function () {
   return canJump
 }
 
+function Spider (game, x, y) {
+  Phaser.Sprite.call(this, game, x, y, 'spider')
+
+    // anchor
+  this.anchor.set(0.5)
+    // animation
+  this.animations.add('crawl', [0, 1, 2], 8, true)
+  this.animations.add('die', [0, 4, 0, 4, 0, 4, 3, 3, 3, 3, 3, 3], 12)
+  this.animations.play('crawl')
+
+    // physic properties
+  this.game.physics.enable(this)
+  this.body.collideWorldBounds = true
+  this.body.velocity.x = Spider.SPEED
+}
+
+Spider.SPEED = 100
+
+// inherit from Phaser.Sprite
+Spider.prototype = Object.create(Phaser.Sprite.prototype)
+Spider.prototype.constructor = Spider
+
 PlayState = {}
 
 PlayState.init = function () {
@@ -57,6 +79,7 @@ PlayState.preload = function () {
   this.game.load.audio('sfx:jump', 'audio/jump.wav')
   this.game.load.spritesheet('coin', 'images/coin_animated.png', 22, 22)
   this.game.load.audio('sfx:coin', 'audio/coin.wav')
+  this.game.load.spritesheet('spider', 'images/spider.png', 42, 32)
 }
 
 PlayState.create = function () {
