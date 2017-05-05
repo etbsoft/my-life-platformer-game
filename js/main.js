6e4717ec086340b4340b0094c502857879926441
Isabel Muñoz <teanocrata@gmail.com>
2017-05-05 11:58:24 +0200

    Make the character face the right direction

--- js/main.js
+++ js/main.js
@@ -17,6 +17,12 @@ Hero.prototype.constructor = Hero
 Hero.prototype.move = function (direction) {
   const SPEED = 200
   this.body.velocity.x = direction * SPEED
+
+  if (this.body.velocity.x < 0) {
+    this.scale.x = -1
+  } else if (this.body.velocity.x > 0) {
+    this.scale.x = 1
+  }
 }
 
 Hero.prototype.jump = function () {
