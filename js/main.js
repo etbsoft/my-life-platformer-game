c8ee0f0e06fec0d68cbf911d4d4374d9d3b767d9
Isabel Muñoz <teanocrata@gmail.com>
2017-05-05 11:15:54 +0200

    Add an animation

--- js/main.js
+++ js/main.js
@@ -105,6 +105,8 @@ PlayState._spawnCharacters = function (data) {
 PlayState._spawnCoin = function (coin) {
   let sprite = this.coins.create(coin.x, coin.y, 'coin')
   sprite.anchor.set(0.5, 0.5)
+  sprite.animations.add('rotate', [0, 1, 2, 1], 6, true) // 6fps, looped
+  sprite.animations.play('rotate')
 }
 
 PlayState._handleInput = function () {
